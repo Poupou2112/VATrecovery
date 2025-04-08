c33624da4c51bdd3ee359b7abaac47ce68f4406c
Poupou2112 <simon.poulain12@gmail.com>
2025-04-08 11:45:48 +0200

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -3,9 +3,11 @@ from ocr_engine import analyze_ticket
 
 def test_ticket_analysis():
     tickets = get_tickets()
+    assert tickets, "Aucun ticket reçu"
+    
     ticket = tickets[0]
     data = analyze_ticket(ticket["file"])
     
-    assert "company_name" in data
-    assert "cif" in data
-    assert data["company_name"] != ""
+    assert "company_name" in data, "Champ 'company_name' manquant"
+    assert "cif" in data, "Champ 'cif' manquant"
+    assert data["company_name"] != "", "'company_name' est vide"
