7fb6bdeccffb7ad855e83d3a45630c528ea2a5a6
Poupou2112 <simon.poulain12@gmail.com>
2025-04-08 11:05:09 +0200

    Update fetch_rydoo.py

--- fetch_rydoo.py
+++ fetch_rydoo.py
@@ -1,13 +1,1 @@
-import os
-import requests
 
-def get_tickets():
-    # Simulation de tickets pour test
-    return [
-        {
-            "file": "app/static/test_ticket1.jpg",  # à adapter selon ton répertoire
-            "email": "comercio@example.com"
-        }
-    ]
-
-# Plus tard, tu remplacerais par une vraie connexion à Rydoo avec OAuth2
