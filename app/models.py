ae604880f3cbdd8ab7eae13604cac1ee6d9bf0b0
Poupou2112 <simon.poulain12@gmail.com>
2025-04-08 22:47:33 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from werkzeug.security import generate_password_hash, check_password_hash
+from secrets import token_urlsafe
+
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
@@ -12,27 +12,10 @@ class User(Base):
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
     client_id = Column(String, nullable=False)
+    api_token = Column(String, unique=True, default=lambda: token_urlsafe(32))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-
-Base = declarative_base()
-
-class Receipt(Base):
-    __tablename__ = "receipts"
-
-    id = Column(Integer, primary_key=True)
-    client_id = Column(String, nullable=False)  # Nouveau champ ajouté
-    file = Column(String)
-    email_sent_to = Column(String)
-    date = Column(String)
-    company_name = Column(String)
-    vat_number = Column(String)
-    price_ttc = Column(Float)
-    email_sent = Column(Boolean, default=False)
-    invoice_received = Column(Boolean, default=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
