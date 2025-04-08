42e9ed283444a4973666ea198b9506a25c3844ba
Poupou2112 <simon.poulain12@gmail.com>
2025-04-08 22:42:28 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,24 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    client_id = Column(String, nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 Base = declarative_base()
 
