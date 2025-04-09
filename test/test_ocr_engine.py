569be756da076332b0a60b8859aa5e3a0b6f121b
Poupou2112 <simon.poulain12@gmail.com>
2025-04-09 13:32:22 +0200

    Create test_ocr_engine.py

---
+++ test/test_ocr_engine.py
@@ -0,0 +1,16 @@
+from app.ocr_engine import extract_info_from_text
+
+def test_extract_info_minimal():
+    text = """
+    UBER FRANCE SAS
+    20/03/2025
+    TVA : 5.32 EUR
+    TTC : 28.45 EUR
+    HT : 23.13 EUR
+    """
+    data = extract_info_from_text(text)
+    assert data["company_name"] == "UBER FRANCE SAS"
+    assert data["date"] == "20/03/2025"
+    assert data["price_ttc"] == 28.45
+    assert data["price_ht"] == 23.13
+    assert data["vat"] == 5.32
