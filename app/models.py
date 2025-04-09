03fcdedd2e5dbfcd07e0e22bea0d913dd3a6cf9d
Poupou2112 <simon.poulain12@gmail.com>
2025-04-09 10:18:06 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import declarative_base
 from secrets import token_urlsafe
+from werkzeug.security import generate_password_hash, check_password_hash
 
 Base = declarative_base()
 
@@ -9,13 +9,13 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False, index=True)
     password_hash = Column(String, nullable=False)
     client_id = Column(String, nullable=False)
     api_token = Column(String, unique=True, default=lambda: token_urlsafe(32))
 
-    def set_password(self, password):
+    def set_password(self, password: str):
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
