a4510d16cc2a8183f4986f63775047dd48607ab2
Poupou2112 <simon.poulain12@gmail.com>
2025-04-09 11:55:25 +0200

    Update test_api_docs.py

--- test/test_api_docs.py
+++ test/test_api_docs.py
@@ -1,4 +1,5 @@
- from fastapi.testclient import TestClient
+
+from fastapi.testclient import TestClient
 from app.main import app
 from app.models import Receipt
 from app.auth import get_current_user
