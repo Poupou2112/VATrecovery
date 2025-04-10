6aefdf1e8eb47f23f5e130b4ff7672a3d3186f84
Poupou2112 <simon.poulain12@gmail.com>
2025-04-10 11:21:32 +0200

    Update test_reminder.py

--- test/test_reminder.py
+++ test/test_reminder.py
@@ -1,20 +1,23 @@
 from app.reminder import send_reminder
-from app.models import Receipt
+from app.models import Receipt, User
 from datetime import datetime, timedelta
 
 def test_send_reminder_mock(monkeypatch):
     class FakeQuery:
-        def filter(self, *args, **kwargs):
-            return self
-        def all(self):
+        def filter(self, *args, **kwargs): return self
+        def filter_by(self, *args, **kwargs): return self
+        def all(self):  # Pour la récupération des reçus à relancer
             return [Receipt(
                 id=1,
                 file="test.jpg",
                 email_sent=True,
                 invoice_received=False,
                 email_sent_to="contact@test.com",
-                created_at=datetime.utcnow() - timedelta(days=10)
+                created_at=datetime.utcnow() - timedelta(days=10),
+                client_id="reclaimy"
             )]
+        def first(self):  # Pour récupérer l'utilisateur
+            return User(api_token="token", client_id="reclaimy")
 
     class FakeSession:
         def query(self, model): return FakeQuery()
