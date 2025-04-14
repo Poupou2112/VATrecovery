9164716da5843c138880afe390861ba307acaa74
Poupou2112 <simon.poulain12@gmail.com>
2025-04-14 20:48:07 +0200

    Update security.py

--- app/security.py
+++ app/security.py
@@ -6,14 +6,14 @@ from app.config import get_settings
 settings = get_settings()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def generate_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
-
 
 def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=30)) -> str:
     to_encode = data.copy()
