0f5717fcc8b263f66c8ebd0018c71196e58c3fa4
Poupou2112 <simon.poulain12@gmail.com>
2025-04-14 12:48:02 +0200

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime, timedelta
 from typing import Optional
 
+from fastapi import APIRouter
 from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
