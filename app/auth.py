0d69ee700210e19a81b265f8e31d0715281e36d4
Poupou2112 <simon.poulain12@gmail.com>
2025-04-14 12:10:25 +0200

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -16,7 +16,6 @@ from slowapi.util import get_remote_address
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi.errors import RateLimitExceeded
 from fastapi import Request
-from slowapi import Throttler
 
 router = APIRouter()
 limiter = Limiter(key_func=get_remote_address)
