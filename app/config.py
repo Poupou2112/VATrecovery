48da4151f1021fc5e111c49a39e3ce89d908fed1
Poupou2112 <simon.poulain12@gmail.com>
2025-04-14 12:25:27 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from pydantic import PostgresDsn, SecretStr, EmailStr
 from pydantic import field_validator
 from typing import Optional, Dict, Any
