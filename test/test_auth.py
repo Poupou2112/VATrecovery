a08aed53b33264a5ac855758be17dc91f4b40516
Poupou2112 <simon.poulain12@gmail.com>
2025-04-14 13:42:17 +0200

    Update test_auth.py

--- test/test_auth.py
+++ test/test_auth.py
@@ -4,10 +4,19 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_invalid_token():
-    response = client.get("/api/receipts", headers={"X-API-Token": "invalid"})
-    assert response.status_code == 401
+def test_login_success(test_user):
+    response = client.post(
+        "/auth/token",
+        data={"username": test_user.email, "password": "test"},  # Assurez-vous que le mot de passe est correct
+    )
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+    assert response.json()["token_type"] == "bearer"
 
-def test_missing_token():
-    response = client.get("/api/receipts")
+def test_login_failure():
+    response = client.post(
+        "/auth/token",
+        data={"username": "wrong@example.com", "password": "wrongpassword"},
+    )
     assert response.status_code == 401
+    assert response.json()["detail"] == "Incorrect email or password"
