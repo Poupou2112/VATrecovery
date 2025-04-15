24aee6313b32c9cc26d9ef9712c8d1b2c38476d7
Poupou2112 <simon.poulain12@gmail.com>
2025-04-15 13:28:51 +0200

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,19 +1,20 @@
 import pytest
+
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.main import app
-from app.database import Base, get_db
 from app.models import Base
+from app.database import get_db
 from app.init_db import init_default_data
 
-Base.metadata.create_all(bind=engine)
-
-# Configuration de la base de test
+DATABASE_URL = "sqlite:///:memory:"  # base en mémoire pour les tests
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
 
 # Setup général : création et initialisation de la base de test
 @pytest.fixture(scope="session", autouse=True)
@@ -50,7 +51,3 @@ def api_token():
 def client():
     with TestClient(app) as c:
         yield c
-def client():
-    with TestClient(app) as c:
-        yield c
-        
