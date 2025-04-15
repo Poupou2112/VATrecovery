ef4c43bbee711110257425731574278c2fc60d26
Poupou2112 <simon.poulain12@gmail.com>
2025-04-15 15:58:57 +0200

    Update test_reminder.py

--- test/test_reminder.py
+++ test/test_reminder.py
@@ -8,7 +8,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import Base
 
-db = SessionLocal()
 engine = create_engine("sqlite:///:memory:", connect_args={"check_same_thread": False})
 Base.metadata.create_all(bind=engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
